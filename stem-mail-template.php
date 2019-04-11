6b9769dd53b02809169aa014ae3bd44869e054ec
Jon Gilkison <jon.gilkison@gmail.com>
2019-04-11 18:32:12 +0700

    Inital commit

---
+++ stem-mail-template.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Plugin Name: Stem Mail Template
+Plugin URI: https://github.com/jawngee/the-wonder
+Description: Model for mail templates
+Author: interfacelab
+Version: 0.1.0
+Author URI: http://interfacelab.io
+*/
+
+
+define('STEM_MAIL_TEMPLATE_DIR', dirname(__FILE__));
+
+if (file_exists(STEM_MAIL_TEMPLATE_DIR.'/vendor/autoload.php')) {
+	require_once STEM_MAIL_TEMPLATE_DIR.'/vendor/autoload.php';
+}
+
+new \Stem\Core\Package(STEM_MAIL_TEMPLATE_DIR);
